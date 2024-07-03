d95d9cf445268dba04b27320f67d519c4c3b0467
AlbuquerquePedro <101055754+AlbuquerquePedro@users.noreply.github.com>
2024-07-03 11:37:16 +0300

    optimizing

--- app.py
+++ app.py
@@ -3,15 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 from dotenv import load_dotenv
+from flask_caching import Cache
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Configuring Flask-Caching
+app.config['CACHE_TYPE'] = 'SimpleCache'  # Consider using 'Redis' or other types for production
+app.config['CACHE_DEFAULT_TIMEOUT'] = 300 # Cache timeout in seconds
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+cache = Cache(app)
 
 class Dream(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -52,9 +57,11 @@ def add_dream():
     new_dream = Dream(title, description)
     db.session.add(new_dream)
     db.session.commit()
+    cache.clear()  # Clear cache when new dream is added
     return dream_schema.jsonify(new_dream)
 
 @app.route('/dreams', methods=['GET'])
+@cache.cached(timeout=60, key_prefix='all_dreams')  # Cache this view for 60 seconds
 def get_dreams():
     all_dreams = Dream.query.all()
     result = dreams_schema.dump(all_dreams)
@@ -63,13 +70,15 @@ def get_dreams():
 @app.route('/analysis', methods=['POST'])
 def add_analysis():
     dream_id = request.json['dream_id']
-    analysis_text = request.json['analysis_text']
-    new_analysis = Analysis(dream_id, analysis_text)
+    analysis_text = request.json['analysis_card']
+    new_analysis = Analysis(dream_type,analysis_text)
     db.session.add(new_analysis)
     db.session.commit()
+    cache.clear()  # Clear cache when new analysis is added
     return analysis_schema.jsonify(new_analysis)
 
 @app.route('/analyses', methods=['GET'])
+@cache.cached(timeout=60, key_prefix='all_analyses')  # Caching this view for 60 seconds
 def get_analyses():
     all_analyses = Analysis.query.all()
     result = analyses_schema.dump(all_analyses)
